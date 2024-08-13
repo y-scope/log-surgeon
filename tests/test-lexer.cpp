52b92512ebc7f320e2382fc9144e8eb4a193e536
SharafMohamed <chrismohamed91@gmail.com>
2024-08-13 07:12:50 -0400

    Formating fixes to text-lexer.cpp

--- tests/test-lexer.cpp
+++ tests/test-lexer.cpp
@@ -5,20 +5,20 @@
 #include <log_surgeon/Schema.hpp>
 #include <log_surgeon/SchemaParser.hpp>
 
-TEST_CASE("Schema", "[Schema]") {
-    SECTION("Add int") {
-        log_surgeon::Schema schema;
-        schema.add_variable("myNumber", "123", -1);
-        auto const schema_ast = schema.release_schema_ast_ptr();
-        REQUIRE(schema_ast->m_schema_vars.size() == 1);
-        auto* schema_var_ast
-                = dynamic_cast<log_surgeon::SchemaVarAST*>(schema_ast->m_schema_vars[0].get());
-        REQUIRE(nullptr != schema_var_ast);
-        REQUIRE(schema_var_ast->m_name == "myNumber");
-        auto* regexASTCat = dynamic_cast<log_surgeon::finite_automata::RegexASTCat<
-                log_surgeon::finite_automata::RegexNFAByteState>*>(schema_var_ast->m_regex_ptr.get()
-        );
-        REQUIRE(nullptr != regexASTCat);
-        REQUIRE(schema.release_schema_ast_ptr()->m_schema_vars.empty());
-    }
+TEST_CASE("Test the Schema class", "[Schema]") {
+    log_surgeon::Schema schema;
+    schema.add_variable("myNumber", "123", -1);
+    auto const schema_ast = schema.release_schema_ast_ptr();
+    REQUIRE(schema_ast->m_schema_vars.size() == 1);
+    REQUIRE(schema.release_schema_ast_ptr()->m_schema_vars.empty());
+
+    auto& schema_var_ast_ptr = schema_ast->m_schema_vars[0];
+    REQUIRE(nullptr != schema_var_ast_ptr);
+    auto& schema_var_ast = dynamic_cast<log_surgeon::SchemaVarAST&>(*schema_var_ast_ptr);
+    REQUIRE("myNumber" == schema_var_ast.m_name);
+
+    auto* regexASTCat = dynamic_cast<log_surgeon::finite_automata::RegexASTCat<
+            log_surgeon::finite_automata::RegexNFAByteState>*>(schema_var_ast->m_regex_ptr.get()
+    );
+    REQUIRE(nullptr != regexASTCat);
 }
