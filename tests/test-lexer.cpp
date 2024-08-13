d17969c1ddd700a64670e50a81b32ae27d66707d
SharafMohamed <chrismohamed91@gmail.com>
2024-08-13 07:05:15 -0400

    clang-tidy fixes for test-lexer.cpp

--- tests/test-lexer.cpp
+++ tests/test-lexer.cpp
@@ -1,22 +1,23 @@
-#include <unistd.h>
-
 #include <catch2/catch_test_macros.hpp>
 
-#include "../src/log_surgeon/Schema.hpp"
+#include <log_surgeon/finite_automata/RegexAST.hpp>
+#include <log_surgeon/finite_automata/RegexNFA.hpp>
+#include <log_surgeon/Schema.hpp>
+#include <log_surgeon/SchemaParser.hpp>
 
 TEST_CASE("Schema", "[Schema]") {
-    log_surgeon::Schema schema;
-
     SECTION("Add int") {
+        log_surgeon::Schema schema;
         schema.add_variable("myNumber", "123", -1);
         auto const schema_ast = schema.release_schema_ast_ptr();
         REQUIRE(schema_ast->m_schema_vars.size() == 1);
-        auto* schemaVarAST
+        auto* schema_var_ast
                 = dynamic_cast<log_surgeon::SchemaVarAST*>(schema_ast->m_schema_vars[0].get());
-        REQUIRE(nullptr != schemaVarAST);
-        REQUIRE(schemaVarAST->m_name == "myNumber");
+        REQUIRE(nullptr != schema_var_ast);
+        REQUIRE(schema_var_ast->m_name == "myNumber");
         auto* regexASTCat = dynamic_cast<log_surgeon::finite_automata::RegexASTCat<
-                log_surgeon::finite_automata::RegexNFAByteState>*>(schemaVarAST->m_regex_ptr.get());
+                log_surgeon::finite_automata::RegexNFAByteState>*>(schema_var_ast->m_regex_ptr.get()
+        );
         REQUIRE(nullptr != regexASTCat);
         REQUIRE(schema.release_schema_ast_ptr()->m_schema_vars.empty());
     }
