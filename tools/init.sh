0ddaa9ecc638f2877e12009d2813876eb9f7ba11
SharafMohamed <chrismohamed91@gmail.com>
2024-08-15 11:03:04 -0400

    Update init.sh to use bash shebang,  use repo root to make script location-agnostic, and only error out if existing .clang-tidy isn't the expected symlink.

--- tools/init.sh
+++ tools/init.sh
@@ -1,3 +1,21 @@
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path/.."
-ln -s tools/yscope-dev-utils/lint-configs/.clang-tidy .clang-tidy
+#!/usr/bin/env bash
+
+# Exit on any error
+set -e
+
+# Error on undefined variable
+set -u
+
+repo_dir="$(git rev-parse --show-toplevel)"
+clang_tidy_config_src_relative_to_repo="tools/yscope-dev-utils/lint-configs/.clang-tidy"
+
+clang_tidy_config_src="${repo_dir}/${clang_tidy_config_src_relative_to_repo}"
+clang_tidy_config_dst="$repo_dir/.clang-tidy"
+if [ ! -e "$clang_tidy_config_dst" ]; then
+    ln -s "$clang_tidy_config_src_relative_to_repo" "$clang_tidy_config_dst"
+else
+    if [ "$(readlink -f "$clang_tidy_config_dst")" != "$(readlink -f "$clang_tidy_config_src")" ];
+    then
+        echo "Unexpected clang-tidy config at $clang_tidy_config_dst"
+    fi
+fi
