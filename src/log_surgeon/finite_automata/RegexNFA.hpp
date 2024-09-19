fe67752f15699c21de093cff66144e53198925ce
Sharaf Mohamed <chrismohamed91@gmail.com>
2024-09-19 10:59:08 -0400

    Add TODO for planned class renaming; Cleanup inline comments. (#37)
    Co-authored-by: Lin Zhihao <59785146+LinZhihao-723@users.noreply.github.com>

--- src/log_surgeon/finite_automata/RegexNFA.hpp
+++ src/log_surgeon/finite_automata/RegexNFA.hpp
@@ -95,6 +95,7 @@ private:
 using RegexNFAByteState = RegexNFAState<RegexNFAStateType::Byte>;
 using RegexNFAUTF8State = RegexNFAState<RegexNFAStateType::UTF8>;
 
+// TODO: rename `RegexNFA` to `NFA`
 template <typename NFAStateType>
 class RegexNFA {
 public:
