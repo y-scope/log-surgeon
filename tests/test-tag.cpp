f72e1205cce4204926edb1b72bff5f273f69b005
SharafMohamed <chrismohamed91@gmail.com>
2024-11-11 11:20:56 -0500

    Better test coverage for tag class.

--- tests/test-tag.cpp
+++ tests/test-tag.cpp
@@ -4,7 +4,19 @@
 
 using log_surgeon::finite_automata::Tag;
 
-TEST_CASE("Test Tag class", "[Tag]") {
-    Tag const tag("uID");
-    REQUIRE("uID" == tag.get_name());
+TEST_CASE("Tag operations", "[Tag]") {
+    SECTION("Basic name retrieval works correctly") {
+        Tag const tag("uID");
+        REQUIRE("uID" == tag.get_name());
+    }
+
+    SECTION("Empty tag name is handled correctly") {
+        Tag const empty_tag("");
+        REQUIRE(empty_tag.get_name().empty());
+    }
+
+    SECTION("Special characters in tag names are preserved") {
+        Tag const special_tag("user.id-123_@");
+        REQUIRE("user.id-123_@" == special_tag.get_name());
+    }
 }
