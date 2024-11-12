30f03ede7b13846fe57eed23c4233753005b7b6d
SharafMohamed <chrismohamed91@gmail.com>
2024-11-12 08:54:37 -0500

    Use braced intiailization in test-tag.cpp.

--- tests/test-tag.cpp
+++ tests/test-tag.cpp
@@ -6,17 +6,17 @@ using log_surgeon::finite_automata::Tag;
 
 TEST_CASE("Tag operations", "[Tag]") {
     SECTION("Basic name retrieval works correctly") {
-        Tag const tag("uID");
+        Tag const tag{"uID"};
         REQUIRE("uID" == tag.get_name());
     }
 
     SECTION("Empty tag name is handled correctly") {
-        Tag const empty_tag("");
+        Tag const empty_tag{""};
         REQUIRE(empty_tag.get_name().empty());
     }
 
     SECTION("Special characters in tag names are preserved") {
-        Tag const special_tag("user.id-123_@");
+        Tag const special_tag{"user.id-123_@"};
         REQUIRE("user.id-123_@" == special_tag.get_name());
     }
 }
