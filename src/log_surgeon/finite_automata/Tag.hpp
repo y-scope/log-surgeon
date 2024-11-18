27c8560a578e7d5dc5b8e5496c393c43798a5b0a
SharafMohamed <chrismohamed91@gmail.com>
2024-11-18 11:38:32 -0500

    Remove new class, going to add it later.

--- src/log_surgeon/finite_automata/Tag.hpp
+++ src/log_surgeon/finite_automata/Tag.hpp
@@ -15,16 +15,6 @@ public:
 private:
     std::string m_name;
 };
-
-class TagPositions {
-public:
-    explicit TagPositions(Tag const* tag) : m_tag{tag} {}
-
-private:
-    Tag const* m_tag;
-    std::vector<uint32_t> start_positions;
-    std::vector<uint32_t> end_positions;
-};
 }  // namespace log_surgeon::finite_automata
 
 #endif  // LOG_SURGEON_FINITE_AUTOMATA_TAG
