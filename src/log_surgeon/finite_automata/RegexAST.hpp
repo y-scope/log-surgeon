aad9eb39b2a2ad149385557b3203caf0ce165dff
Sharaf Mohamed <sharaf.mohamed@mail.utoronto.ca>
2024-11-19 15:11:49 -0500

    Fix spacing.
    Co-authored-by: Lin Zhihao <59785146+LinZhihao-723@users.noreply.github.com>

--- src/log_surgeon/finite_automata/RegexAST.hpp
+++ src/log_surgeon/finite_automata/RegexAST.hpp
@@ -902,7 +902,6 @@ void RegexASTCapture<NFAStateType>::add_to_nfa(RegexNFA<NFAStateType>* nfa, NFAS
             = nfa->new_state_with_positive_tagged_transition(m_tag.get(), end_state);
     nfa->set_root(capture_group_start_state);
     m_group_regex_ast->add_to_nfa_with_negative_tags(nfa, state_with_positive_tagged_transition);
-
     nfa->set_root(root);
 }
 
