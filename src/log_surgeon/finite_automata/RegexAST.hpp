57cf69ba73563e30e45398c0b35f16a8fb16284e
SharafMohamed <chrismohamed91@gmail.com>
2025-01-29 22:53:41 -0500

    Limit workflow to ubuntu-22.04.

--- src/log_surgeon/finite_automata/RegexAST.hpp
+++ src/log_surgeon/finite_automata/RegexAST.hpp
@@ -141,7 +141,7 @@ protected:
                 [](Capture const* capture) { return fmt::format("<~{}>", capture->get_name()); }
         );
 
-        std::string const negative_captures_string{
+        auto const negative_captures_string{
                 fmt::format("{}", fmt::join(transformed_negative_captures, ""))
         };
 
