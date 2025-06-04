02ae78e041ca929d0b216bc207326cbe9a8fe6fd
SharafMohamed <chrismohamed91@gmail.com>
2025-06-04 06:27:52 -0400

    Update error handling for add_variable and add_delimiters; Add unit-tests for this update.

--- src/log_surgeon/Schema.cpp
+++ src/log_surgeon/Schema.cpp
@@ -12,11 +12,17 @@ Schema::Schema(std::string const& schema_file_path)
 
 auto Schema::add_variable(std::string_view const var_schema, int const priority) const -> void {
     auto const schema_ast{SchemaParser::try_schema_string(var_schema)};
+    if (schema_ast->m_schema_vars.empty()) {
+        throw std::invalid_argument("No variable found in schema string");
+    }
     m_schema_ast->add_schema_var(std::move(schema_ast->m_schema_vars[0]), priority);
 }
 
 auto Schema::add_delimiters(std::string_view const delimiters_schema) const -> void {
     auto const schema_ast{SchemaParser::try_schema_string(delimiters_schema)};
+    if (schema_ast->m_delimiters.empty()) {
+        throw std::invalid_argument("No delimiters found in schema string");
+    }
     m_schema_ast->add_delimiters(std::move(schema_ast->m_delimiters[0]));
 }
 }  // namespace log_surgeon
