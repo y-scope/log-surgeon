8843de62a21e251658d0056bcad1c2afb6439589
davidlion <david.lion@yscope.com>
2025-06-09 00:37:57 -0400

    Apply clang-format changes.

--- src/log_surgeon/Token.hpp
+++ src/log_surgeon/Token.hpp
@@ -58,8 +58,8 @@ public:
      */
     [[nodiscard]] auto get_length() const -> uint32_t;
 
-    [[nodiscard]] auto get_reversed_reg_positions(reg_id_t const reg_id
-    ) const -> std::vector<finite_automata::PrefixTree::position_t> {
+    [[nodiscard]] auto get_reversed_reg_positions(reg_id_t const reg_id) const
+            -> std::vector<finite_automata::PrefixTree::position_t> {
         return m_reg_handler.get_reversed_positions(reg_id);
     }
 
