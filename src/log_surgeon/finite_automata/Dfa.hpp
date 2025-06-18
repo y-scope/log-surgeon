b99d5e68326d9e8707dced1ce6a9697edef2f047
Sharaf Mohamed <sharaf.mohamed@mail.utoronto.ca>
2025-06-18 11:18:30 -0400

    Update src/log_surgeon/finite_automata/Dfa.hpp
    Co-authored-by: davidlion <davidlion2@protonmail.com>

--- src/log_surgeon/finite_automata/Dfa.hpp
+++ src/log_surgeon/finite_automata/Dfa.hpp
@@ -94,8 +94,8 @@ public:
      */
     auto process_state(TypedDfaState const* dfa_state, uint32_t curr_pos) -> void;
 
-    auto set(TypedDfaState const* prev_state) -> TypedDfaState const* {
-        m_curr_state = prev_state;
+    auto set_curr_state(TypedDfaState const* state) -> TypedDfaState const* {
+        m_curr_state = state;
         return m_curr_state;
     }
 
