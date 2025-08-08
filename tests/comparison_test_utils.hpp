8e4b4843161842679e59b9221a5bfa18b4d60910
SharafMohamed <SharafMohamed@users.noreply.github.com>
2025-08-08 09:25:36 -0400

    Use concept to enforce template type in comparison utils.

--- tests/comparison_test_utils.hpp
+++ tests/comparison_test_utils.hpp
@@ -9,12 +9,15 @@
 #include <catch2/catch_test_macros.hpp>
 
 namespace log_surgeon::tests {
+template <typename T>
+concept StronglyThreeWayComparable = std::three_way_comparable<T, std::strong_ordering>;
+
 /**
  * Tests comparison operators when `lhs` == `rhs`.
  * @param lhs Value on the lhs of the operator.
  * @param rhs Value on the rhs of the operator.
  */
-template <typename T>
+template <StronglyThreeWayComparable T>
 auto test_equal(T const& lhs, T const& rhs) -> void;
 
 /**
@@ -22,7 +25,7 @@ auto test_equal(T const& lhs, T const& rhs) -> void;
  * @param lhs Value on the lhs of the operator.
  * @param rhs Value on the rhs of the operator.
  */
-template <typename T>
+template <StronglyThreeWayComparable T>
 auto test_greater_than(T const& lhs, T const& rhs) -> void;
 
 /**
@@ -30,17 +33,17 @@ auto test_greater_than(T const& lhs, T const& rhs) -> void;
  * @param lhs Value on the lhs of the operator.
  * @param rhs Value on the rhs of the operator.
  */
-template <typename T>
+template <StronglyThreeWayComparable T>
 auto test_less_than(T const& lhs, T const& rhs) -> void;
 
 /**
  * Tests operators `<=>`, `==`, `!=`, `<`, `<=`, `>`, `>=` for every pair of elements in the vector.
  * @param ordered_vector Vector where elements are ordered to be strictly ascending.
  */
-template <typename T>
+template <StronglyThreeWayComparable T>
 auto pairwise_comparison_of_strictly_ascending_vector(std::vector<T> const& ordered_vector) -> void;
 
-template <typename T>
+template <StronglyThreeWayComparable T>
 auto test_equal(T const& lhs, T const& rhs) -> void {
     REQUIRE((lhs <=> rhs) == std::strong_ordering::equal);
     REQUIRE(lhs == rhs);
@@ -58,7 +61,7 @@ auto test_equal(T const& lhs, T const& rhs) -> void {
     REQUIRE_FALSE(rhs > lhs);
 }
 
-template <typename T>
+template <StronglyThreeWayComparable T>
 auto test_greater_than(T const& lhs, T const& rhs) -> void {
     REQUIRE((lhs <=> rhs) == std::strong_ordering::greater);
     REQUIRE(lhs != rhs);
@@ -76,7 +79,7 @@ auto test_greater_than(T const& lhs, T const& rhs) -> void {
     REQUIRE_FALSE(rhs > lhs);
 }
 
-template <typename T>
+template <StronglyThreeWayComparable T>
 auto test_less_than(T const& lhs, T const& rhs) -> void {
     REQUIRE((lhs <=> rhs) == std::strong_ordering::less);
     REQUIRE(lhs != rhs);
@@ -94,7 +97,7 @@ auto test_less_than(T const& lhs, T const& rhs) -> void {
     REQUIRE_FALSE(rhs < lhs);
 }
 
-template <typename T>
+template <StronglyThreeWayComparable T>
 auto pairwise_comparison_of_strictly_ascending_vector(std::vector<T> const& ordered_vector)
         -> void {
     for (size_t i{0}; i < ordered_vector.size(); i++) {
