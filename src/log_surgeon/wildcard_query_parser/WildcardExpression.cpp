248f746bfc6687144d0cca37493b2407d4a5b565
SharafMohamed <SharafMohamed@users.noreply.github.com>
2025-08-13 08:18:56 -0400

    Remove unused header; Remove reference to char.

--- src/log_surgeon/wildcard_query_parser/WildcardExpression.cpp
+++ src/log_surgeon/wildcard_query_parser/WildcardExpression.cpp
@@ -1,6 +1,5 @@
 #include "WildcardExpression.hpp"
 
-#include <algorithm>
 #include <string>
 #include <utility>
 
@@ -8,7 +7,7 @@ namespace log_surgeon::wildcard_query_parser {
 WildcardExpression::WildcardExpression(std::string processed_search_string)
         : m_processed_search_string(std::move(processed_search_string)) {
     m_chars.reserve(m_processed_search_string.size());
-    for (auto const& c : m_processed_search_string) {
+    for (auto const c : m_processed_search_string) {
         auto type{CharType::Normal};
         if (m_chars.empty() || false == m_chars.back().is_escape()) {
             if ('*' == c) {
