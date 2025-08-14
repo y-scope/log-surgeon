9e676793ddcc42af19c27e16f9ffd66482babdb5
SharafMohamed <SharafMohamed@users.noreply.github.com>
2025-08-14 10:18:39 -0400

    Rename CharType to Type; Move Type to be public; Use WildcardCharacter::Type in WildcardExpression.

--- src/log_surgeon/wildcard_query_parser/WildcardCharacter.hpp
+++ src/log_surgeon/wildcard_query_parser/WildcardCharacter.hpp
@@ -6,30 +6,30 @@
 namespace log_surgeon::wildcard_query_parser {
 class WildcardCharacter {
 public:
-    WildcardCharacter(char const value, CharType const type) : m_value{value}, m_type{type} {}
-
-    [[nodiscard]] auto value() const -> char { return m_value; }
-
-    [[nodiscard]] auto is_greedy_wildcard() const -> bool {
-        return CharType::GreedyWildcard == m_type;
-    }
-
-    [[nodiscard]] auto is_non_greedy_wildcard() const -> bool {
-        return CharType::NonGreedyWildcard == m_type;
-    }
-
-    [[nodiscard]] auto is_escape() const -> bool { return CharType::Escape == m_type; }
-
-private:
-    enum class CharType : uint8_t {
+    enum class Type : uint8_t {
         Normal,
         GreedyWildcard,
         NonGreedyWildcard,
         Escape
     };
 
+    WildcardCharacter(char const value, Type const type) : m_value{value}, m_type{type} {}
+
+    [[nodiscard]] auto value() const -> char { return m_value; }
+
+    [[nodiscard]] auto is_greedy_wildcard() const -> bool {
+        return Type::GreedyWildcard == m_type;
+    }
+
+    [[nodiscard]] auto is_non_greedy_wildcard() const -> bool {
+        return Type::NonGreedyWildcard == m_type;
+    }
+
+    [[nodiscard]] auto is_escape() const -> bool { return Type::Escape == m_type; }
+
+private:
     char m_value;
-    CharType m_type;
+    Type m_type;
 };
 }  // namespace log_surgeon::wildcard_query_parser
 
--- src/log_surgeon/wildcard_query_parser/WildcardExpression.cpp
+++ src/log_surgeon/wildcard_query_parser/WildcardExpression.cpp
@@ -10,14 +10,14 @@ WildcardExpression::WildcardExpression(std::string processed_search_string)
         : m_processed_search_string(std::move(processed_search_string)) {
     m_chars.reserve(m_processed_search_string.size());
     for (auto const c : m_processed_search_string) {
-        auto type{CharType::Normal};
+        auto type{WildcardCharacter::Type::Normal};
         if (m_chars.empty() || false == m_chars.back().is_escape()) {
             if ('*' == c) {
-                type = CharType::GreedyWildcard;
+                type = WildcardCharacter::Type::GreedyWildcard;
             } else if ('?' == c) {
-                type = CharType::NonGreedyWildcard;
+                type = WildcardCharacter::Type::NonGreedyWildcard;
             } else if ('\\' == c) {
-                type = CharType::Escape;
+                type = WildcardCharacter::Type::Escape;
             }
         }
         m_chars.emplace_back(c, type);
