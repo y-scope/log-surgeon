9adf7e9501d88d23f0e508a0647ad3c09cbaeaaa
SharafMohamed <SharafMohamed@users.noreply.github.com>
2025-08-14 11:01:56 -0400

    Improve naming of wildcard character test cases.

--- tests/test-wildcard-character.cpp
+++ tests/test-wildcard-character.cpp
@@ -15,7 +15,7 @@ using log_surgeon::wildcard_query_parser::WildcardCharacter;
  * @ingroup unit_tests_wildcard_character
  * @brief Tests a `WildcardCharacter` that stores a normal character.
  */
-TEST_CASE("normal", "[WildcardCharacter]") {
+TEST_CASE("normal_expression_character", "[WildcardCharacter]") {
     WildcardCharacter const wildcard_character{'a', WildcardCharacter::Type::Normal};
     REQUIRE('a' == wildcard_character.value());
     REQUIRE_FALSE(wildcard_character.is_greedy_wildcard());
@@ -27,7 +27,7 @@ TEST_CASE("normal", "[WildcardCharacter]") {
  * @ingroup unit_tests_wildcard_character
  * @brief Tests a `WildcardCharacter` that stores a greedy wildcard.
  */
-TEST_CASE("greedy_wildcard", "[WildcardCharacter]") {
+TEST_CASE("greedy_wildcard_expression_character", "[WildcardCharacter]") {
     WildcardCharacter const wildcard_character{'*', WildcardCharacter::Type::GreedyWildcard};
     REQUIRE('*' == wildcard_character.value());
     REQUIRE(wildcard_character.is_greedy_wildcard());
@@ -39,7 +39,7 @@ TEST_CASE("greedy_wildcard", "[WildcardCharacter]") {
  * @ingroup unit_tests_wildcard_character
  * @brief Tests a `WildcardCharacter` that stores a non-greedy wildcard.
  */
-TEST_CASE("non_greedy_wildcard", "[WildcardCharacter]") {
+TEST_CASE("non_greedy_wildcard_expression_character", "[WildcardCharacter]") {
     WildcardCharacter const wildcard_character{'?', WildcardCharacter::Type::NonGreedyWildcard};
     REQUIRE('?' == wildcard_character.value());
     REQUIRE_FALSE(wildcard_character.is_greedy_wildcard());
@@ -51,7 +51,7 @@ TEST_CASE("non_greedy_wildcard", "[WildcardCharacter]") {
  * @ingroup unit_tests_wildcard_character
  * @brief Tests a `WildcardCharacter` that stores an escape.
  */
-TEST_CASE("escape", "[WildcardCharacter]") {
+TEST_CASE("escape_expression_character", "[WildcardCharacter]") {
     WildcardCharacter const wildcard_character{'\\', WildcardCharacter::Type::Escape};
     REQUIRE('\\' == wildcard_character.value());
     REQUIRE_FALSE(wildcard_character.is_greedy_wildcard());
---
+++ tests/test-wildcard-expression.cpp
@@ -0,0 +1,18 @@
+#include <log_surgeon/wildcard_query_parser/WildcardExpression.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+/**
+ * @defgroup unit_tests_wildcard_expression `WildcardExpression` unit tests.
+ * @brief Unit tests for `WildcardExpression` to verify storage and type predicate methods.
+
+ * These unit tests contain the `WildcardExpression` tag.
+ */
+
+/**
+ * @ingroup unit_tests_wildcard_expression
+ * @brief Tests an empty `WildcardExpression`.
+ */
+TEST_CASE("empty", "[WildcardExpression]") {
+    
+}
