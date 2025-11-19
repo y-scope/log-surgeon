6ec9356dedf18fcc0d66831a3df0dedbdadb892c
SharafMohamed <SharafMohamed@users.noreply.github.com>
2025-11-19 04:48:30 -0500

    Remove unused method from capture.

--- src/log_surgeon/finite_automata/Capture.hpp
+++ src/log_surgeon/finite_automata/Capture.hpp
@@ -2,18 +2,12 @@
 #define LOG_SURGEON_FINITE_AUTOMATA_CAPTURE
 
 #include <string>
-#include <string_view>
 #include <utility>
 
 namespace log_surgeon::finite_automata {
 class Capture {
 public:
     explicit Capture(std::string name) : m_name{std::move(name)} {}
-
-    auto set_context(std::string rule_name, uint32_t pos) {
-        m_rule_name = std::move(rule_name);
-        m_pos = pos;
-    }
 
     [[nodiscard]] auto get_name() const -> std::string const& { return m_name; }
 
