45640348e62ab461edd22ef59599c101d4c498f9
SharafMohamed <SharafMohamed@users.noreply.github.com>
2025-12-18 19:27:30 -0500

    Add namespace around global methods.

--- examples/common.cpp
+++ examples/common.cpp
@@ -12,39 +12,41 @@
 using namespace std;
 using namespace log_surgeon;
 
-static auto check_input(std::vector<std::string> const& args) -> int {
-    int ret{0};
-    if (2 != args.size()) {
-        ret = 1;
-        cout << "Not enough arguments.\n";
-    } else if (filesystem::path const f{args[0]}; false == filesystem::exists(f)) {
-        ret = 2;
-        cout << "Schema file does not exist.\n";
-    } else if (filesystem::path const f{args[1]}; false == filesystem::exists(f)) {
-        ret = 3;
-        cout << "Input file does not exist.\n";
-    }
-    if (0 != ret) {
-        cout << "usage: <path to schema file> <path to input log file>\n";
-    }
-    return ret;
-}
-
-static auto print_timestamp_loglevel(LogEventView const& event, uint32_t loglevel_id) -> void {
-    Token* const timestamp{event.get_timestamp()};
-    Token* loglevel{nullptr};
-    if (nullptr != timestamp) {
-        if (auto const& vec{event.get_variables(loglevel_id)}; false == vec.empty()) {
-            loglevel = vec[0];
+namespace {
+    auto check_input(std::vector<std::string> const& args) -> int {
+        int ret{0};
+        if (2 != args.size()) {
+            ret = 1;
+            cout << "Not enough arguments.\n";
+        } else if (filesystem::path const f{args[0]}; false == filesystem::exists(f)) {
+            ret = 2;
+            cout << "Schema file does not exist.\n";
+        } else if (filesystem::path const f{args[1]}; false == filesystem::exists(f)) {
+            ret = 3;
+            cout << "Input file does not exist.\n";
         }
+        if (0 != ret) {
+            cout << "usage: <path to schema file> <path to input log file>\n";
+        }
+        return ret;
     }
-    if (nullptr != timestamp) {
-        cout << "timestamp: ";
-        cout << timestamp->to_string_view();
+
+    auto print_timestamp_loglevel(LogEventView const& event, uint32_t loglevel_id) -> void {
+        Token* const timestamp{event.get_timestamp()};
+        Token* loglevel{nullptr};
+        if (nullptr != timestamp) {
+            if (auto const& vec{event.get_variables(loglevel_id)}; false == vec.empty()) {
+                loglevel = vec[0];
+            }
+        }
+        if (nullptr != timestamp) {
+            cout << "timestamp: ";
+            cout << timestamp->to_string_view();
+        }
+        if (nullptr != loglevel) {
+            cout << ", loglevel:";
+            cout << loglevel->to_string_view();
+        }
+        cout << "\n";
     }
-    if (nullptr != loglevel) {
-        cout << ", loglevel:";
-        cout << loglevel->to_string_view();
-    }
-    cout << "\n";
-}
+}  // namespace
--- examples/common.hpp
+++ examples/common.hpp
@@ -7,10 +7,12 @@
 
 #include <log_surgeon/LogEvent.hpp>
 
-static auto check_input(std::vector<std::string> const& args) -> int;
+namespace {
+    auto check_input(std::vector<std::string> const& args) -> int;
 
-static auto print_timestamp_loglevel(
-        log_surgeon::LogEventView const& event, uint32_t loglevel_id
-) -> void;
+    auto print_timestamp_loglevel(
+            log_surgeon::LogEventView const& event, uint32_t loglevel_id
+    ) -> void;
+}  // namespace
 
 #endif  // COMMON_HPP
