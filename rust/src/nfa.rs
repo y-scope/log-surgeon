d89c1c2edd252d8e3fa9f8ba5245a6aab0f23579
Adrian <adrian@yscope.com>
2026-07-07 08:19:28 -0400

    nfa, dfa...

---
+++ rust/src/nfa.rs
@@ -0,0 +1,152 @@
+//! Based on Angelo Borsotti and Ulya Trafimovich. 2022. A closer look at TDFA.
+//! - <https://re2c.org/2022_borsotti_trofimovich_a_closer_look_at_tdfa.pdf>
+//! - <https://arxiv.org/abs/2206.01398>
+//!
+
+mod graph_dot_output;
+mod regex_construction;
+mod search_decomposition;
+
+use std::borrow::Cow;
+use std::collections::BTreeSet;
+
+pub use search_decomposition::Path;
+pub use search_decomposition::PathComponent;
+
+use crate::interval_tree::IntervalTree;
+use crate::parsing_spec::RuleIdx;
+use crate::parsing_spec::SubRule;
+
+#[derive(Debug, Clone)]
+pub struct Tnfa {
+	states: Vec<NfaState>,
+	tags: BTreeSet<CaptureTag>,
+}
+
+#[derive(Debug, Clone)]
+pub struct NfaState {
+	/// ID and also an index into an [`Nfa`]'s list of states.
+	pub idx: NfaIdx,
+	/// By construction, an NFA state should have _either_
+	/// symbol transitions or priority-ordered spontaneous transitions.
+	pub transitions: Transitions,
+	pub maybe_accepts_for_rule: Option<RuleIdx>,
+	/// Not strictly needed, but useful for debugging (including DOT output).
+	pub name: Cow<'static, str>,
+}
+
+/// Newtype wrapper around a `usize` index.
+#[derive(Debug, Clone, Copy, Eq, Ord, PartialEq, PartialOrd, Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct NfaIdx(usize);
+
+#[derive(Debug, Clone)]
+pub enum Transitions {
+	Interval(IntervalTree<u32, NfaIdx>),
+	/// Untagged epsilon transitions.
+	Spontaneous(Vec<NfaIdx>),
+	Tagged {
+		tag: CaptureTag,
+		positive: bool,
+		target: NfaIdx,
+	},
+}
+
+#[derive(Debug, Clone, Eq, Ord, PartialEq, PartialOrd)]
+pub enum CaptureTag {
+	Start(SubRule),
+	Stop(SubRule),
+}
+
+impl Tnfa {
+	pub const BLANK: Self = Self {
+		states: Vec::new(),
+		tags: BTreeSet::new(),
+	};
+
+	pub fn new() -> Self {
+		Self {
+			states: vec![NfaState {
+				idx: NfaIdx::BEGIN,
+				name: Cow::Borrowed("begin"),
+				transitions: Transitions::Spontaneous(Vec::new()),
+				maybe_accepts_for_rule: None,
+			}],
+			tags: BTreeSet::new(),
+		}
+	}
+
+	pub fn tags(&self) -> &BTreeSet<CaptureTag> {
+		&self.tags
+	}
+
+	fn new_state<LikeString>(&mut self, name: LikeString) -> NfaIdx
+	where
+		LikeString: Into<Cow<'static, str>>,
+	{
+		let idx: NfaIdx = NfaIdx(self.states.len());
+		let state: NfaState = NfaState {
+			idx,
+			name: name.into(),
+			transitions: Transitions::Spontaneous(Vec::new()),
+			maybe_accepts_for_rule: None,
+		};
+		self.states.push(state);
+		idx
+	}
+}
+
+impl std::ops::Index<NfaIdx> for Tnfa {
+	type Output = NfaState;
+
+	fn index(&self, i: NfaIdx) -> &Self::Output {
+		&self.states[i.0]
+	}
+}
+
+impl std::ops::IndexMut<NfaIdx> for Tnfa {
+	fn index_mut(&mut self, i: NfaIdx) -> &mut Self::Output {
+		&mut self.states[i.0]
+	}
+}
+
+impl NfaState {
+	pub fn is_accepting(&self) -> bool {
+		self.maybe_accepts_for_rule.is_some()
+	}
+}
+
+impl NfaIdx {
+	pub const BEGIN: NfaIdx = Self(0);
+}
+
+impl std::fmt::Display for NfaIdx {
+	fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		fmt.write_fmt(format_args!("q{}", self.0))
+	}
+}
+
+impl Transitions {
+	pub fn len(&self) -> usize {
+		match self {
+			Self::Interval(transitions) => transitions.len(),
+			Self::Spontaneous(transitions) => transitions.len(),
+			Self::Tagged { .. } => 1,
+		}
+	}
+
+	fn successors(&self) -> Box<dyn Iterator<Item = NfaIdx> + '_> {
+		match self {
+			Self::Interval(transitions) => Box::new(transitions.iter().map(|(_interval, target)| *target)),
+			Self::Spontaneous(transitions) => Box::new(transitions.iter().copied()),
+			Self::Tagged { target, .. } => Box::new(std::iter::once(*target)),
+		}
+	}
+}
+
+impl CaptureTag {
+	pub fn sub_rule(&self) -> &SubRule {
+		let (Self::Start(sub_rule) | Self::Stop(sub_rule)) = self;
+		sub_rule
+	}
+}
